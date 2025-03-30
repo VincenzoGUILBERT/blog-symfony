a205e4cf7adc44c9d975b42143c9305740be4eae
Vincent Guilbert <vincentguilbert13@protonmail.com>
2025-03-30 16:17:47 +0200

    Create Comment entity and add dataset

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Comment;
 use App\Entity\Post;
 use App\Entity\User;
 use Faker\Factory;
@@ -12,32 +13,45 @@ use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 class AppFixtures extends Fixture
 {
 
-    public function __construct(private UserPasswordHasherInterface $hasher){}
+    public function __construct(private UserPasswordHasherInterface $hasher) {}
 
     public function load(ObjectManager $manager): void
     {
         $faker = Factory::create('fr_FR');
+        $usersArray = [];
 
-        for ($i=0; $i < 10; $i++) { 
+        for ($i = 0; $i < 10; $i++) {
             $user = new User();
             $hashedPassword = $this->hasher->hashPassword($user, 'password');
             $user->setUsername($faker->userName())
-                 ->setEmail($faker->email())
-                 ->setPassword($hashedPassword)
-                 ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 year', 'now')))
+                ->setEmail($faker->email())
+                ->setPassword($hashedPassword)
+                ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 year', 'now')))
             ;
 
             $manager->persist($user);
+            array_push($usersArray, $user);
+        }
 
-            for ($j=0; $j < mt_rand(3,10); $j++) { 
-                $post = new Post();
-                $post->setTitle($faker->sentence())
-                     ->setContent($faker->paragraph(10, true))
-                     ->setAuthor($user)
-                     ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 year', 'now')))
+        for ($j = 0; $j < 50; $j++) {
+            $post = new Post();
+            $post->setTitle($faker->sentence())
+                ->setContent($faker->paragraph(10, true))
+                ->setAuthor($usersArray[mt_rand(0, 9)])
+                ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 year', 'now')))
+            ;
+
+            $manager->persist($post);
+
+            for ($k = 0; $k < mt_rand(1, 10); $k++) {
+                $comment = new Comment();
+                $comment->setContent($faker->paragraph(mt_rand(1, 5)))
+                    ->setCreatedAt(\DateTimeImmutable::createFromMutable($faker->dateTimeBetween('-1 year', 'now')))
+                    ->setAuthor($usersArray[mt_rand(0, 9)])
+                    ->setPost($post)
                 ;
 
-                $manager->persist($post);
+                $manager->persist($comment);
             }
         }
 
--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -2,6 +2,8 @@
 
 namespace App\Entity;
 
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PostRepository;
@@ -41,6 +43,17 @@ class Post
     #[ORM\ManyToOne(inversedBy: 'posts')]
     #[ORM\JoinColumn(nullable: false)]
     private ?User $author = null;
+
+    /**
+     * @var Collection<int, Comment>
+     */
+    #[ORM\OneToMany(targetEntity: Comment::class, mappedBy: 'post', orphanRemoval: true)]
+    private Collection $comments;
+
+    public function __construct()
+    {
+        $this->comments = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -91,6 +104,36 @@ class Post
     public function setAuthor(?User $author): static
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Comment>
+     */
+    public function getComments(): Collection
+    {
+        return $this->comments;
+    }
+
+    public function addComment(Comment $comment): static
+    {
+        if (!$this->comments->contains($comment)) {
+            $this->comments->add($comment);
+            $comment->setPost($this);
+        }
+
+        return $this;
+    }
+
+    public function removeComment(Comment $comment): static
+    {
+        if ($this->comments->removeElement($comment)) {
+            // set the owning side to null (unless already changed)
+            if ($comment->getPost() === $this) {
+                $comment->setPost(null);
+            }
+        }
 
         return $this;
     }
